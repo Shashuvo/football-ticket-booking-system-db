ad424308792b05b6fe529d8e3a516e4b08e43344
Shashuvo <shahariat2810@gmail.com>
2026-06-15 21:02:44 +0600

    query 3 added

--- QUERY.sql
+++ QUERY.sql
@@ -104,4 +104,11 @@ WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 -- =========================================================================
 SELECT user_id, full_name, email
 FROM Users
-WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%'
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+-- =========================================================================
+-- QUERY 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+-- =========================================================================
+SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') as systematic_status 
+FROM Bookings
+WHERE payment_status IS NULL;
